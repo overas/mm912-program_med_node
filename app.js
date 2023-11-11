9bef91cd8deede24454826bbb4704c97feaa5a80
overas <37910044+overas@users.noreply.github.com>
2023-11-11 19:00:11 +0100

    .....

---
+++ app.js
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+
+console.log(
+  chalk.blue("Alle ") +
+    chalk.magenta("barna ") +
+    chalk.cyan("kikket ") +
+    chalk.redBright("ut ") +
+    chalk.greenBright("fra ") +
+    chalk.yellowBright("skyskraperen ") +
+    chalk.blueBright("unntatt ") +
+    chalk.magentaBright("Inger, ") +
+    chalk.cyanBright("hun ")
+    chalk.whiteBright("trodde ")
+    chalk.black("hun ")
+    chalk.blue("hadde ")
+    chalk.red("vinger.")
+);
